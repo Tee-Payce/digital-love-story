019ecc48e5dfcc277ad2c4ae05fa501593e5b55c
BDODEV <psiziba@bdo.co.zw>
2026-01-26 16:23:29 +0200

    basename

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-import React, { useState, useEffect } from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [darkMode]);
-
   return (
-    <div className="font-sans bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-500">
-      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
-      <main className="scroll-smooth">
-        <section id="hero"><Hero /></section>
-        <section id="about"><About /></section>
-        <section id="projects"><Projects /></section>
-        <section id="contact"><Contact /></section>
-      </main>
-    </div>
+    <Router basename="/digital-love-story">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
